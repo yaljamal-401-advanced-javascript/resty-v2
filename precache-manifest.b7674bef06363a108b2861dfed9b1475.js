0524587693605d3efe0564b03c1269eb386828e1
yaljamal <yazan.aljamal@gmail.com>
2020-06-30 16:20:14 +0300

    Updates

---
+++ precache-manifest.b7674bef06363a108b2861dfed9b1475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d03771556713078587b271f79b68a7",
+    "url": "/resty-v2/index.html"
+  },
+  {
+    "revision": "469dcea67562a0b75365",
+    "url": "/resty-v2/static/css/main.da9b510d.chunk.css"
+  },
+  {
+    "revision": "e645f4b0330be63a2161",
+    "url": "/resty-v2/static/js/2.a5cb4acf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty-v2/static/js/2.a5cb4acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469dcea67562a0b75365",
+    "url": "/resty-v2/static/js/main.6121b7af.chunk.js"
+  },
+  {
+    "revision": "beb92be5cd5d0921aea6",
+    "url": "/resty-v2/static/js/runtime-main.afacf9fc.js"
+  }
+]);
